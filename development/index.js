2162635a92bb3af9f1fdbee21209fedf3caafe15
ImaneMF01 <imane.mniefilali@bbc.co.uk>
2021-08-12 15:37:18 +0000

    Switch between HDR and non-HDR content + Play video from the right timestamp

--- development/index.js
+++ development/index.js
@@ -31,8 +31,7 @@ require (['bigscreenplayer/bigscreenplayer'], function(BigscreenPlayer){
   button.style.position='absolute'
   button.style.background='transparent'
   button.style.border='none'
-  //button.style.backgroundImage='http://localhost:3008/playsdr.png'
-  //button.style.backgroundImage=playfilename
+
   button.style.backgroundSize="cover"
   button.style.backgroundPosition="center"
   button.style.width="80px"
@@ -98,31 +97,28 @@ require (['bigscreenplayer/bigscreenplayer'], function(BigscreenPlayer){
 
   /////////Logo
 
-var img = document.createElement('img')
-img.style.position='absolute'
-img.style.paddingTop= "60px"
-img.style.paddingLeft="30px"
-//img.style.width="100px";
-//img.style.height="100px";
+  var img = document.createElement('img')
+  img.style.position='absolute'
+  img.style.paddingTop= "60px"
+  img.style.paddingLeft="30px"
+  //img.style.width="100px";
+  //img.style.height="100px";
 
-document.body.appendChild(img)
+  document.body.appendChild(img)
 
-function setimghdr(ishdr){
-    
-  var imgsdr='./bbc_logo.png'
-  var imghdr='./bbc_hdrlogo.png'
+  function setimghdr(ishdr){
+      
+    var imgsdr='./bbc_logo.png'
+    var imghdr='./bbc_hdrlogo.png'
 
-    if(ishdr){
-      filename=imghdr
-      }
-    else{
-      filename=imgsdr
-      }
-  img.src=filename
-}
-
-////Some Text
-
+      if(ishdr){
+        filename=imghdr
+        }
+      else{
+        filename=imgsdr
+        }
+    img.src=filename
+  }
 
   ////////set HDR on/off
 
@@ -131,124 +127,84 @@ function setimghdr(ishdr){
     setpausebuttonhdr(isHdr)
     setimghdr(isHdr)
     settxthdr(isHdr)
-    bigscreenPlayer.customiseSubtitles({hdr:isHdr})
+
+    const currentTime = bigscreenPlayer.getCurrentTime();
+
+    bigscreenPlayer.tearDown();
+    loadPlayer(isHdr, currentTime);
   }
 
   window.setHdr = setHdr   /// make visible in global (document) scope
 
 
   
-//add some text: Video Title
-var t = document.createElement("INPUT")
+  //add some text: Video Title
+  var t = document.createElement("INPUT")
 
 
-//t.setAttribute("type", "text")
-t.setAttribute("value", "    Playing now: Peaky Blinders ")
-t.style.position='absolute'
-//t.style.color= 'White'
-t.style.width="300px"
-t.style.height="40px"
-//t.style.background= '#F54997'
-t.style.left= '960px'
-t.style.fontWeight='bold'
-t.style.fontSize='20px'
-t.style.top= "25px"
+  //t.setAttribute("type", "text")
+  t.setAttribute("value", "    Playing now: Black Narcissus ")
+  t.style.position='absolute'
+  //t.style.color= 'White'
+  t.style.width="300px"
+  t.style.height="40px"
+  //t.style.background= '#F54997'
+  t.style.left= '960px'
+  t.style.fontWeight='bold'
+  t.style.fontSize='20px'
+  t.style.top= "25px"
 
 
-function settxthdr(ishdr){
-    
-  var btxtsdr='#F54997'
-  var btxthdr='#a84f85'
+  function settxthdr(ishdr){
+      
+    var btxtsdr='#F54997'
+    var btxthdr='#a84f85'
 
-  var txtsdr='white'
-  var txthdr ='#b8b8b8'
+    var txtsdr='white'
+    var txthdr ='#b8b8b8'
 
-    if(ishdr){
-      bcolor= btxthdr
-      tcolor= txthdr
-      }
-    else{
-      bcolor= btxtsdr
-      tcolor= txtsdr
-      }
-  t.style.background=bcolor
-  t.style.color= tcolor
-}
-document.body.appendChild(t)
-
-
+      if(ishdr){
+        bcolor= btxthdr
+        tcolor= txthdr
+        }
+      else{
+        bcolor= btxtsdr
+        tcolor= txtsdr
+        }
+    t.style.background=bcolor
+    t.style.color= tcolor
+  }
+  document.body.appendChild(t)
 
 
-
-  
-  
-  // var pausefilenamesdr="url('http://localhost:3008/pausesdr.png')"
-  // var pausefilenamehdr="url('http://localhost:3008/pausehdr.png')"
-
-  // var pausefilename=pausefilenamesdr
-  
-  // if (hdrgui){
-  //     pausefilename=pausefilenamehdr
-  // }
-
-//   var pausebutton = document.createElement('button')
-//   pausebutton.style.position='absolute'
-//   pausebutton.style.background='transparent'
-//   pausebutton.style.left= '80px'
-//   pausebutton.style.width="80px"
-//   pausebutton.style.height="80px"
-//   pausebutton.style.border='none'
-//   pausebutton.style.backgroundImage=pausefilename
-//   pausebutton.style.backgroundSize="cover"
-//   pausebutton.style.backgroundPosition="center"
-//   pausebutton.style.marginTop= "600px"
-
-
-//   pausebutton.addEventListener('click', function(){
-//     bigscreenPlayer.pause();
-    
-//   });
-
-
-//   var img = document.createElement('img')
-  
-//   var imgsdr='http://localhost:3008/bbc_logo.png'
-//   var imghdr='http://localhost:3008/bbc_hdrlogo.png'
-
-//   var logofilename=imgsdr
-  
-//     if (hdrgui){
-//        logofilename=imghdr
-//     }
-
-// img.style.position='absolute'
-// img.style.paddingTop= "10px"
-// img.style.paddingLeft="10px"
-// // img.style.width="100px";
-// // img.style.height="100px";
-
-// img.src=logofilename
-
-// // var img = document.querySelector("img"); 
-// // img.src = "https://picsum.photos/200/301";
-
-// //document.getElementById('body').appendChild(img);
-// document.body.appendChild(img)
-
-// document.body.appendChild(pausebutton)
-
-
-  const getStreamURL = function () {
+  const getStreamURL = function (hdr = false) {
     return new Promise(function (resolve) {
-        window._antie_callback_ms_p05qtr4g = function (data) {
+        //window._antie_callback_ms_p05qtr4g = function (data) {
+
+        const supplier = hdr ? 'mf_akamai_uhd' : 'mf_akamai';
+
+        // The above is the same as:
+        // let supplier; 
+        // if (hdr)
+        //   supplier = 'mf_akamai_uhd';
+        // else 
+        //   supplier = 'mf_akamai';
+        
+        window._antie_callback_ms_m000qzd1 = function (data) {  
             console.log(data);
 
-            const connection = data.media[0].connection;
+            let akamaiDashConnection;
 
-            const akamaiDashConnection = connection.find(
-              (conn) => 
-                conn.supplier === 'mf_akamai' && conn.transferFormat === 'dash'
-            );
+            for (const media of data.media) {
+              akamaiDashConnection = media.connection.find(
+                (conn) => 
+                  conn.supplier === supplier && conn.transferFormat === 'dash' 
+              );
+
+              if (akamaiDashConnection)
+                break;
+            }
+
             if (!akamaiDashConnection)
               throw new Error('Could not find Akamai DASH stream.');
 
@@ -258,7 +214,23 @@ document.body.appendChild(t)
         
         script = document.createElement('script');
         script.type = 'text/javascript';
-        script.src = 'https://securegate.iplayer.bbc.co.uk/mediaselector/6/select/version/2.0/vpid/p05qtr4g/format/json/mediaset/iptv-all/jsfunc/_antie_callback_ms_p05qtr4g/proto/https';
+        ///UHD////
+       // script.src = 'https://securegate.iplayer.bbc.co.uk/mediaselector/6/select/version/2.0/vpid/m000qzd1/format/json/mediaset/iptv-uhd/jsfunc/_antie_callback_ms_m000qzd1/proto/https';
+        //script.src = 'https://open.live.bbc.co.uk/mediaselector/6/select/version/2.0/vpid/m000qzd1/format/json/mediaset/iptv-sd/jsfunc/_antie_callback_ms_m000qzd1/proto/https';
+        
+        ////HD///https://open.live.bbc.co.uk/mediaselector/6/select/version/2.0/mediaset/iptv-sd/vpid/m000qzd1/
+        // script.src = 'https://securegate.iplayer.bbc.co.uk/mediaselector/6/select/version/2.0/vpid/m000qzd1/format/json/mediaset/iptv-all/jsfunc/_antie_callback_ms_m000qzd1/proto/https';
+        //script.src = 'https://securegate.iplayer.bbc.co.uk/mediaselector/6/select/version/2.0/vpid/m000qzd1/format/json/mediaset/iptv-all/jsfunc/_antie_callback_ms_m000qzd1/proto/https';
+        /////
+
+         //script.src = 'https://securegate.iplayer.bbc.co.uk/mediaselector/6/select/version/2.0/vpid/m000qzd1/format/json/mediaset/iptv-all/jsfunc/_antie_callback_ms_m000qzd1/proto/https';
+        // script.src = 'https://securegate.iplayer.bbc.co.uk/mediaselector/6/select/version/2.0/vpid/p05qtr4g/format/json/mediaset/iptv-all/jsfunc/_antie_callback_ms_p05qtr4g/proto/https';
+       // script.src = 'https://securegate.iplayer.bbc.co.uk/mediaselector/6/select/version/2.0/vpid/m000qw33/format/json/mediaset/iptv-uhd/jsfunc/_antie_callback_ms_m000qw33/proto/https';
+
+        const mediaset = hdr ? 'iptv-uhd' : 'iptv-all';
+
+        script.src = `https://securegate.iplayer.bbc.co.uk/mediaselector/6/select/version/2.0/vpid/m000qzd1/format/json/mediaset/${mediaset}/jsfunc/_antie_callback_ms_m000qzd1/proto/https`;
+
         document.head.appendChild(script);
         document.head.removeChild(script);
     });
@@ -279,48 +251,52 @@ document.body.appendChild(t)
     console.log('Transforming subtitles failed...')
   }})
 
-  getStreamURL()
-    .then(function (streamURL) {
-      let minimalData = {
-        initialPlaybackTime: 30,
-        media: {
-          //captions: [ {url: "http://localhost:3008/sub.xml", cdn: 'test'}],
-          //captions: [ {url: "http://localhost:3008/doctorWhoSubs.xml",
-          captions: [ {url: "./Peaky_Blinders_S2E2.xml",
-          //sevenWorldsOnePlanetSub
-          cdn: 'test'}],
-          type: 'application/dash+xml',
-          mimeType: 'video/mp4',
-          kind: 'video',
-          urls: [{
-            // Content from DASH IF testing assests (used in their reference player)
-            // https://reference.dashif.org/dash.js/v2.9.2/samples/dash-if-reference-player/index.htm
-            url: streamURL,
-            //url:'//vod-uhd-uk-live.akamaized.net/r1brygt0-pips-pid-m000c5x9/scmaf_abr_full_uhd/rd/iptv_uhd_v1_master.mpd?__gda__=1623683111_249b213fc4d025851866281848cb27b7',
-            //url:'http://vod-dash-uk-live.akamaized.net/usp/auth/vod/piff_abr_full_sd_ad/1844d7-b04l8yty/vf_b04l8yty_b9eaabe7-1e59-4250-953f-94450da8f50e.ism.hlsv2.ism/iptv_sdlow_abr_v2_dash_master.mpd?__gda__=1623684338_8e1a67cf14ac338cf144bfa2e94f990a',
-            //url:'http://vod-dash-uk-live.akamaized.net/usp/auth/vod/piff_abr_full_sd_ad/1844d7-b04l8yty/vf_b04l8yty_b9eaabe7-1e59-4250-953f-94450da8f50e.ism/pc_hd_abr_v2_dash_master.mpd?__gda__=1623684626_88b60bdf2ac4daa541baea52c1cc7e93',
-           // url:'http://vod-hls-uk-live.akamaized.net/usp/auth/vod/piff_abr_full_hd/1844d7-b04l8yty/vf_b04l8yty_f893eb14-a343-4d51-bc51-d04fa75d3b98.ism.hlsv2.ism/iptv_hd_abr_v1_hls_master.m3u8?__gda__=1623699488_a71a443b619278ff48e7204d595a4cc1',
-            //url:'https://b4-eqsl-bbc.live.bidi.net.uk/vod-dash-uk/usp/auth/vod/piff_abr_full_hd/d9b872-m000pb14/vf_m000pb14_b17efb13-ebfe-4bb6-a483-de8b8a485e18.ism/iptv_hd_bvq_abr_v1_dash_master.mpd?at=9qkH4cCz0216fbd99766556cea4088658727cd30ff9dce3d5c2c272eafcc0',
-            //url:'https://vod-uhd-uk-live.akamaized.net/r1brygt0-pips-pid-m000c5x9/scmaf_abr_full_uhd/rd/iptv_uhd_v1_master.mpd?__gda__=1621871788_46151ce24b596aa903c4c755081aa766',
-            //uhd Seven Worlds One Planet url:'https://vod-uhd-uk-live.akamaized.net/r1brygt0-pips-pid-m000c5x9/scmaf_abr_full_uhd/rd/iptv_uhd_v1_master.mpd?__gda__=1621883321_0e1c4ae19c0fa4e0e0e2709008310e6b',
-            //url:'http://vod-dash-uk-live.akamaized.net/usp/auth/vod/piff_abr_full_hd/1844d7-b04l8yty/vf_b04l8yty_f893eb14-a343-4d51-bc51-d04fa75d3b98.ism/iptv_hd_abr_v1_dash_master.mpd?__gda__=1624544537_f465c0dc9fe06172ee2e6a30e9a68458',
-            //url:'http://vod-dash-uk-live.akamaized.net/usp/auth/vod/piff_abr_full_hd/1844d7-b04l8yty/vf_b04l8yty_f893eb14-a343-4d51-bc51-d04fa75d3b98.ism/iptv_hd_abr_v1_dash_master.mpd?__gda__=1625151430_5f7e7fb83d230b2704133531c3bebda0',
-            cdn: 'dash.akamaized.net'
-          }]
-        }
-      };
-
-      bigscreenPlayer.init(playbackElement, minimalData, windowType, enableSubtitles,
-        {
-          onSuccess: function () {
-           // bigscreenPlayer.toggleDebug();
-            setUpCaptionsContainerCSS();
-            setHdr(false)
-          },
-          onError: function () {
-            bigscreenPlayer.toggleDebug();
-            DebugTool.info('Initialisation failed.')
+  const loadPlayer = function(hdr = false, initialPlaybackTime = 0) {
+    getStreamURL(hdr)
+      .then(function (streamURL) {
+        let minimalData = {
+          initialPlaybackTime: initialPlaybackTime,
+          media: {
+            //captions: [ {url: "http://localhost:3008/doctorWhoSubs.xml",
+            //captions: [ {url: "./Peaky_Blinders_S2E2.xml",
+            captions: [ {url: "./Black_Narcissus_S1E2.xml",
+            cdn: 'test'}],
+            type: 'application/dash+xml',
+            mimeType: 'video/mp4',
+            kind: 'video',
+            urls: [{
+              // Content from DASH IF testing assests (used in their reference player)
+              // https://reference.dashif.org/dash.js/v2.9.2/samples/dash-if-reference-player/index.htm
+              url: streamURL,
+              //url:'http://vod-dash-uk-live.akamaized.net/usp/auth/vod/piff_abr_full_hd/1844d7-b04l8yty/vf_b04l8yty_f893eb14-a343-4d51-bc51-d04fa75d3b98.ism/iptv_hd_abr_v1_dash_master.mpd?__gda__=1625151430_5f7e7fb83d230b2704133531c3bebda0',
+              cdn: 'dash.akamaized.net'
+            }],
           }
+        };
+  
+        bigscreenPlayer.init(playbackElement, minimalData, windowType, enableSubtitles,
+          {
+            onSuccess: function () {
+              // bigscreenPlayer.toggleDebug();
+
+              // The syntax below is equal to:
+              // bigscreenPlayer.customiseSubtitles({ hdr: hdr });
+              bigscreenPlayer.customiseSubtitles({ hdr });
+
+              setplaybuttonhdr(hdr);
+              setpausebuttonhdr(hdr);
+              setimghdr(hdr);
+              settxthdr(hdr);
+
+              setUpCaptionsContainerCSS();
+            },
+            onError: function () {
+              bigscreenPlayer.toggleDsebug();
+              DebugTool.info('Initialisation failed.')
+            }
+        });
       });
-    });
+  }
+
+  loadPlayer(false);
 })
